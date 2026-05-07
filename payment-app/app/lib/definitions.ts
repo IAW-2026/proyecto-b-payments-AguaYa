5ac1f61351ca578206a87a5561094d1fd190636d
Agustin Condori <agustincondori40@gmail.com>
2026-05-07 08:33:29 -0300

    commit inicial y defini las entidades en definitions.ts

---
+++ payment-app/app/lib/definitions.ts
@@ -0,0 +1,28 @@
+export type Payment = {
+  payment_id: string;
+
+  order_id: string;
+  buyer_id: string;
+  seller_id: string;
+
+  amount: number;
+
+  //estado interno del sistema
+  payment_status: "pending" | "cancelled" | "approved" | "rejected" | "expired";
+
+  created_at: Date;
+  updated_at: Date;
+
+  //datos externos de MercadoPago
+
+  mp_payment_id?: string;
+  mp_status?: string;
+  mp_payment_method?: string;
+  mp_payment_date?: Date;
+};
+export type Payment_Admin = {
+  clerk_user_id: string;
+  payment_admin_id: string;
+  name: string;
+  created_at: Date; //es util para poder responder¿Quién agregó este admin?¿Cuándo obtuvo permisos?¿Desde cuándo tiene acceso?
+};
